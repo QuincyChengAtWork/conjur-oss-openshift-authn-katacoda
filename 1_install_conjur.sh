7d3ab37df86ac2916602aa54f8180c572369acaf
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-05-07 15:09:48 +0800

    Update 1_install_conjur.sh

--- 1_install_conjur.sh
+++ 1_install_conjur.sh
@@ -10,15 +10,37 @@ then
     oc create namespace "$CONJUR_NAMESPACE"
 fi
 
-helm init
-oc create serviceaccount --namespace kube-system tiller
-oc create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade
+# Install Helm Client
+curl -ks https://storage.googleapis.com/kubernetes-helm/helm-v2.13.1-linux-amd64.tar.gz | tar xz
+sudo mv linux-amd64/helm /usr/local/bin
+sudo chmod a+x /usr/local/bin/helm
+helm init --client-only
 
+sleep 2
+
+
+### Install Helm Server
+export TILLER_NAMESPACE=tiller
+oc new-project tiller
+oc project tiller
+oc policy add-role-to-user edit "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:tiller:tiller --as=system:admin
+oc process -f https://quincycheng.github.io/tiller-template.yaml -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" | oc create -f -
+oc rollout status deployment tiller
+
+sleep 2
+
+
+### Update Chart
 helm repo add cyberark https://cyberark.github.io/helm-charts
 helm repo update
 
-sleep 5
+sleep 2
+
+### 
+oc project conjur
+oc policy add-role-to-user edit "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
+
 
 helm install cyberark/conjur-oss \
     --set ssl.hostname=$CONJUR_HOSTNAME_SSL,dataKey="$(docker run --rm cyberark/conjur data-key generate)",authenticators="authn-k8s/dev\,authn" \
